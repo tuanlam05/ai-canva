6169405b831c32a5ae1889d7300d53648dbcfc40
Rishi Sood <soodrishi04@gmail.com>
2026-09-18 22:42:05 +1000

    remove Firebase pointers to deleted files

--- client/src/lib/firebase.ts
+++ client/src/lib/firebase.ts
@@ -6,7 +6,6 @@ const firebaseConfig = {
   apiKey: "AIzaSyA1ZIcRxu0o6DGGCiNQ3Z9OMzZVcqsLr_U",
   authDomain: "ai-canva-8bd1d.firebaseapp.com",
   projectId: "ai-canva-8bd1d",
-  storageBucket: "ai-canva-8bd1d.firebasestorage.app",
   messagingSenderId: "974926894968",
   appId: "1:974926894968:web:b38cb77a7075c5b6f25a8d"
 };
